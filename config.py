2c4fed85f0c9113a621a3069e45715d2c3d4322f
Angelika Romanovskaya <angelikaromanovskaya@gmail.com>
2026-01-18 22:07:47 +0300

    add config

---
+++ config.py
@@ -0,0 +1,14 @@
+# Параметры управления
+BASE_SPEED = 0.3
+TURN = 0.2
+LINE_THRESHOLD = 0.4  # Порог для обнаружения линии (0-1)
+
+# Параметры изображения
+IMG_WIDTH = 64
+IMG_HEIGHT = 64
+
+# Параметры остановки
+MAX_NO_LINE = 150
+
+# Пути
+MODEL_PATH = "line_follower_1.xml"
